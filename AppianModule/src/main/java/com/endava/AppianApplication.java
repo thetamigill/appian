92dd3719f060939070948aa27964a462dcb104d8
danish.shakil <danishshakil15@yahoo.com>
2022-03-03 17:41:19 -0500

    project setup

---
+++ AppianModule/src/main/java/com/endava/AppianApplication.java
@@ -0,0 +1,13 @@
+package com.endava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppianApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppianApplication.class, args);
+    }
+
+}
---
+++ AppianModule/src/main/java/com/endava/controller/AppianController.java
@@ -0,0 +1,7 @@
+package com.endava.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AppianController {
+}
